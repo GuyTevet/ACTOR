1e697e7c7843488a143956936c32bfc8f230b362
GuyTevet <guy.tvt@gmail.com>
2021-11-09 11:40:16 +0200

    eval pipeline - fix for remote run

--- src/evaluate/evaluate_cvae.py
+++ src/evaluate/evaluate_cvae.py
@@ -7,7 +7,7 @@ def main():
     dataset = parameters["dataset"]
     print(dataset)
     if dataset in ["ntu13", "humanact12"]:
-        from .gru_eval import evaluate
+        from src.evaluate.gru_eval import evaluate
         evaluate(parameters, folder, checkpointname, epoch, niter)
     elif dataset in ["uestc"]:
         from .stgcn_eval import evaluate
